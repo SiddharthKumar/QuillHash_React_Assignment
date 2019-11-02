878145b95e01294f51de944098d15a9ecdb66324
kesharwanir193@gmail.com <rajat@wdmtech.com>
2019-11-02 17:25:43 +0530

    User token set in local storage

--- src/App.js
+++ src/App.js
@@ -4,17 +4,38 @@ import './App.css';
 import Router from './Router/index'
 import { createBrowserHistory as createHistory } from 'history'
 
+// users
+import users from './Component/Core/Users.json';
+const userList = users.userList;
+
 class App extends Component {
   state = {
     validUser: true,
   }
+
+  static checkValidUser = () => {
+    console.log("Check User");
+    const token = localStorage.getItem("token");
+    let valid = false;
+    userList.map(user => {
+      if (user.token === token) {
+        valid = true
+      }
+      return true;
+    });
+    return { validUser: valid }
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return App.checkValidUser();
+  }
   render() {
+    const history = createHistory();
     if (!this.state.validUser) {
-      const history = createHistory()
       history.push('/login');
     }
     return (
-      <Router />
+      <Router history={history} />
     );
   }
 }
--- src/Component/Core/login/login.js
+++ src/Component/Core/login/login.js
@@ -1,26 +1,111 @@
 import React, { Component } from 'react';
 import './login.css';
+
+// user list
+import users from '../Users.json';
+const userList = users.userList;
+
 export class login extends Component {
+    state = {
+        eMail: "",
+        password: "",
+        rememberMe: false,
+        showErrorMsg: false,
+        errorMsg: "",
+    }
+
+    handleStateChange = (type) => (event) => {
+        let value = null;
+        switch (type) {
+            case "eMail":
+                value = event.target.value;
+                break;
+            case "password":
+                value = event.target.value;
+                break;
+            case "rememberMe":
+                value = !this.state.rememberMe;
+                break;
+            default:
+                value = null;
+        }
+        if (value != null) {
+            this.setState({
+                [type]: value,
+            });
+            if (this.state.showErrorMsg) {
+                this.setState({
+                    showErrorMsg: false,
+                    errorMsg: "",
+                });
+            }
+        }
+    }
+
+    handleLogin = (event) => {
+        event.preventDefault();
+        userList.map(user => {
+            if (user.eMail === this.state.eMail && user.password === this.state.password) {
+                localStorage.setItem("token", user.token);
+                localStorage.setItem("rememberMe", this.state.rememberMe);
+                this.props.history.push("");
+            }
+            return true;
+        });
+        this.setState({
+            showErrorMsg: true,
+            errorMsg: "Username or password is invalid",
+        });
+    }
+
     render() {
+        const { eMail, password, rememberMe } = this.state;
         return (
             <div className='loginBody'>
+                {(this.state.showErrorMsg) ?
+                    <>
+                        <label htmlFor="exampleInputEmail1" style={{ color: "red" }}>{this.state.errorMsg}</label>                    </>
+                    : null
+                }
                 <form>
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required />
+                        <input
+                            type="email"
+                            className="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder="Enter email"
+                            value={eMail}
+                            onChange={this.handleStateChange("eMail")}
+                            required />
                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
-                        <div className="form-group">
+                    <div className="form-group">
                         <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" required/>
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Password"
+                            value={password}
+                            onChange={this.handleStateChange("password")}
+                            required />
                     </div>
                     <div className="form-group form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                        <input
+                            type="checkbox"
+                            className="form-check-input"
+                            checked={rememberMe ? "checked" : ""}
+                            onChange={this.handleStateChange("rememberMe")}
+                        />
+                        <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
                     </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                        onClick={this.handleLogin}
+                    >Submit</button>
                 </form>
-                
+
             </div>
         )
     }
