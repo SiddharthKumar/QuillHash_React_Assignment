dc4ac8307fe3e3cb46f2be5fc45011cf0028fa1c
unknown <siddharthkumar801@gmail.com>
2019-11-05 15:13:23 +0530

    favourite btn add in progress

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -5,8 +5,6 @@ const footer = () => {
     return (
         <React.Fragment>
             <footer className="page-footer font-small blue footer ">
-
-              
                 <div className="footer-copyright text-center py-3">© 2018 Copyright:
                  <a href="">Quillhash</a>
                 </div>
