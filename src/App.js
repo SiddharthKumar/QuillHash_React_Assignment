aa67026edbb27e9b626bf727497548cfeebc98d9
kesharwanir193@gmail.com <rajat@wdmtech.com>
2019-11-05 17:11:19 +0530

    Login/Logout fix with comment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from 'react'
 
+import { createBrowserHistory } from "history";
 import Router from './Router/Router';
 
+/**
+ * get users list from json file
+ */
+import users from './Component/Core/Users.json';
+const userList = users.userList;
+
+/**
+ * create history object 
+ */
+const history = createBrowserHistory();
+
 const App = (props) => {
+  
+  /**
+   * Check User authentication
+   */
+  if (!checkValidUser()) {
+    history.push("/login")
+  }
+
   return (
     <Router />
   )
+}
+
+
+/**
+ * User authentication
+ * 
+ * return true/false
+ */
+const checkValidUser = () => {
+  const token = localStorage.getItem("token");
+  let valid = false;
+  userList.map(user => {
+    if (user.token === token) {
+      valid = true
+    }
+    return true;
+  });
+  return valid
 }
 
 export default App
